5856c525727b79063d10400a417b1a4ed5821e37
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-08-23 11:19:42 +0600

    function add

--- js/app.js
+++ js/app.js
@@ -37,3 +37,50 @@ getPlayerNameVlueById('btn-di-maria', 'di-maria');
 getPlayerNameVlueById('btn-jake', 'jake');
 getPlayerNameVlueById('btn-zidane-iqbal', 'zidane-iqbal');
 getPlayerNameVlueById('btn-renato-sanches', 'renato-sanches');
+
+
+
+function getInputValueById(valueId) {
+    const inputField = document.getElementById(valueId);
+    const inputAmountString = inputField.value;
+    const inputAmount = parseFloat(inputAmountString);
+    inputField.value = '';
+    return inputAmount;
+}
+
+function getSetElementValueById(elementId) {
+    const elementField = document.getElementById(elementId);
+    const elementAmountString = elementField.innerText;
+    const elementAmount = parseFloat(elementAmountString);
+    return elementAmount;
+
+}
+
+function setTextElementValueById(elementId, newValue) {
+    const elementField = document.getElementById(elementId);
+    elementField.innerText = newValue;
+}
+
+
+document.getElementById('btn-calculate').addEventListener('click', function () {
+    const playerInputAmount = getInputValueById('player-field');
+    const size = selectAllPlayer();
+
+    const setTotalAmount = size * playerInputAmount;
+    document.getElementById('total-expenses-player').innerText = setTotalAmount
+
+})
+
+document.getElementById('btn-calculate-total').addEventListener('click', function () {
+    const managerCostAmount = getInputValueById('manager-field')
+    const coachCostFieldAmount = getInputValueById('coach-field')
+
+    const totalManagerAndCoachCost = managerCostAmount + coachCostFieldAmount;
+
+    const previousTotalAmount = getSetElementValueById('balance-total')
+    const expensesPlayerAmount = getSetElementValueById('total-expenses-player')
+
+    const totalAmount = previousTotalAmount + totalManagerAndCoachCost + expensesPlayerAmount;
+    setTextElementValueById('balance-total', totalAmount);
+
+})
