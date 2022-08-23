5bcd7e65972e0ce987911a04989735a49635519c
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-08-23 10:54:55 +0600

    header h1 changed

--- js/app.js
+++ js/app.js
@@ -20,3 +20,20 @@ function getPlayerNameVlueById(btnId, nameId) {
     })
 }
 
+function selectAllPlayer() {
+    const listItem = document.querySelectorAll('#player-name-list li');
+    const selectName = [];
+    for (const list of listItem) {
+        selectName.push(list);
+    }
+
+    const allLength = selectName.length;
+    return allLength;
+}
+
+getPlayerNameVlueById('btn-messi', 'messi');
+getPlayerNameVlueById('btn-neymar', 'neymar');
+getPlayerNameVlueById('btn-di-maria', 'di-maria');
+getPlayerNameVlueById('btn-jake', 'jake');
+getPlayerNameVlueById('btn-zidane-iqbal', 'zidane-iqbal');
+getPlayerNameVlueById('btn-renato-sanches', 'renato-sanches');
